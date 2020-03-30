37577ba76f76195da76aa416924ece2724bf0608
Rebeca Lima <rebeca.lsantos31@gmail.com>
2020-03-30 12:55:22 -0300

    :art: create structure

--- Model/Client.php
+++ Model/Client.php
@@ -7,5 +7,51 @@ class Client
     private $name;
     private $dependentes;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDependentes()
+    {
+        return $this->dependentes;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param mixed $dependentes
+     */
+    public function setDependentes($dependentes)
+    {
+        $this->dependentes = $dependentes;
+    }
 }
